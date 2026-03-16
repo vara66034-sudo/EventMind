c0c93a3441cd6e3cb2ef4bffdc29940d1593881e
nk <anastasiakovalevich24@gmail.com>
2026-03-16 11:40:20 +0500

    Update __manifest__.py

--- backend /app/eventmind_ai/eventmind_recommend/__manifest__.py
+++ backend /app/eventmind_ai/eventmind_recommend/__manifest__.py
@@ -1,1 +1,12 @@
+{
+    'name': 'EventMind Recommendations',
+    'version': '1.0',
+    'summary': 'Personalized event recommendations based on user interests',
+    'depends': ['event', 'eventmind_tags'],  # зависимость от модуля Полины
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': False,
+}
 
