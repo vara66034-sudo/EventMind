51f344a814d06c94c7445b048a3b4aca287f2dd3
nk <anastasiakovalevich24@gmail.com>
2026-03-24 20:06:00 +0500

    Create App.jsx

---
+++ fronten/src/App.jsx
@@ -0,0 +1,35 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import ProtectedRoute from './components/ProtectedRoute';
+import Home from './pages/Home';
+import Interests from './pages/Interests';
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route
+                    path="/"
+                    element={
+                        <ProtectedRoute>
+                            <Home />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path="/interests"
+                    element={
+                        <ProtectedRoute>
+                            <Interests />
+                        </ProtectedRoute>
+                    }
+                />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
