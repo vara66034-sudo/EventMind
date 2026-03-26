a0f523793ae9f32b95f53e6c1eb0670001c5c3d4
nk <anastasiakovalevich24@gmail.com>
2026-03-26 17:24:25 +0500

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import EventsPage from './pages/EventsPage';
+import EventDetailPage from './pages/EventDetailPage';
+import ProfilePage from './pages/ProfilePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import EditProfilePage from './pages/EditProfilePage';
+import './styles/global.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <main style={{ minHeight: '80vh' }}>
+          <Routes>
+            {/* Главная страница - события */}
+            <Route path="/" element={<EventsPage />} />
+            
+            {/* Детали события */}
+            <Route path="/events/:id" element={<EventDetailPage />} />
+            
+            {/* Личный кабинет */}
+            <Route path="/profile" element={<ProfilePage />} />
+            
+            {/* Редактирование профиля */}
+            <Route path="/profile/edit" element={<EditProfilePage />} />
+            
+            {/* Авторизация */}
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
