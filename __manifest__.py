06a71866b68f68312b78ad6b0a2a3c43427c288b
vara66034-sudo <vara66034@gmail.com>
2026-03-26 22:11:38 +0500

    добавил manifest

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'EventMind2',
+    'version': '1.0',
+    'author': 'Your Name',
+    'category': 'Custom',
+    'summary': 'EventMind2 ',
+    'depends': [],
+    'data': [],
+    'installable': True,
+}
