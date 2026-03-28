e833951b445b5c235c6079bdb8b0012ce884e868
vara66034-sudo <vara66034@gmail.com>
2026-03-28 13:43:09 +0500

    коммит

--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -120,31 +120,80 @@ const EventsPage = () => {
   const mockEvents = [
     {
       id: 1,
-      name: 'Концерт любимой группы',
-      date_begin: '2024-04-15 19:00:00',
-      location: 'Москва, Крокус Сити Холл',
-      image: "https://i.ytimg.com/vi/TggWGrkaXg4/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AH-CYAC0AWKAgwIABABGC4gSSh_MA8=&rs=AOn4CLCIhRkLoiPgkzR01KdUMGOtQvrNqA",
-    },
-    {
-      id: 2,
-      name: 'Мастер-класс по фотографии',
-      date_begin: '2024-04-20 14:00:00',
-      location: 'Санкт-Петербург, Лофт Проект Этажи',
-      image: "https://avatars.mds.yandex.net/get-vertis-journal/4212087/IMG_2922.webp_1723909471198/orig",
-    },
-    {
-      id: 3,
-      name: 'IT Конференция 2024',
-      date_begin: '2024-05-01 10:00:00',
-      location: 'Онлайн',
-      image: "https://avatars.mds.yandex.net/i?id=ddc8aecec77d15591bb6d2518efc62bd_l-5268915-images-thumbs&n=13",
+      name: 'EventMind AI Hackathon 2026',
+      date_begin: '2026-05-20 10:00:00',
+      location: 'Екатеринбург, Технопарк',
+      image: 'https://images.unsplash.com/photo-1504384308090-c894fdcc538d?w=800'
     },
     {
       id: 4,
-      name: 'Джазовый вечер',
-      date_begin: '2024-04-18 20:00:00',
-      location: 'Екатеринбург',
-      image: "https://avatars.mds.yandex.net/get-afishanew/5098259/a95faf50c562e9510cbef1b5b0129dd2/960x690_noncrop",
+      name: 'Python & Data Science Meetup',
+      date_begin: '2026-06-05 19:00:00',
+      location: 'Екатеринбург, Ельцин Центр',
+      image: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?w=800'
+    },
+    {
+      id: 5,
+      name: 'Ural CyberSecurity Forum',
+      date_begin: '2026-07-10 09:00:00',
+      location: 'Екатеринбург-ЭКСПО',
+      image: 'https://images.unsplash.com/photo-1550751827-4bd374c3f58b?w=800'
+    }, 
+    {
+      id: 6,
+      name: 'EventMind AI Hackathon 2026',
+      date_begin: '2026-04-15 19:00:00',
+      location: 'Екатеринбург, Технопарк "Университетский"',
+      image: 'https://images.unsplash.com/photo-1504384308090-c894fdcc538d?w=800',
+    },
+    {
+      id: 2,
+      name: 'Python & Data Science Meetup',
+      date_begin: '2026-04-20 14:00:00',
+      location: 'Екатеринбург, Ельцин Центр',
+      image: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?w=800',
+    },
+    {
+      id: 7,
+      name: 'Ural CyberSecurity Forum 2026',
+      date_begin: '2026-05-01 10:00:00',
+      location: 'Екатеринбург-ЭКСПО',
+      image: 'https://images.unsplash.com/photo-1550751827-4bd374c3f58b?w=800',
+    },
+    {
+      id: 8,
+      name: 'Frontend Intensive: React & Next.js',
+      date_begin: '2026-04-18 20:00:00',
+      location: 'Онлайн (Zoom )',
+      image: 'https://images.unsplash.com/photo-1587620962725-abab7fe55159?w=800',
+    },
+    {
+      id: 9,
+      name: 'DevOps & Cloud Infrastructure Day',
+      date_begin: '2026-05-12 11:00:00',
+      location: 'Екатеринбург, БЦ "Высоцкий"',
+      image: 'https://images.unsplash.com/photo-1451187580459-43490279c0fa?w=800',
+    },
+    {
+      id: 10,
+      name: 'GameDev Weekend: Unity & Unreal',
+      date_begin: '2026-05-25 12:00:00',
+      location: 'Екатеринбург, Коворкинг "Names"',
+      image: 'https://images.unsplash.com/photo-1552820728-8b83bb6b773f?w=800',
+    },
+    {
+      id: 11,
+      name: 'Mobile Conf: iOS & Android Trends',
+      date_begin: '2026-06-05 10:00:00',
+      location: 'Екатеринбург, Хаятт Ридженси',
+      image: 'https://images.unsplash.com/photo-1512941937669-90a1b58e7e9c?w=800',
+    },
+    {
+      id: 12,
+      name: 'Product Management Workshop',
+      date_begin: '2026-06-15 18:30:00',
+      location: 'Онлайн',
+      image: 'https://images.unsplash.com/photo-1531403009284-440f080d1e12?w=800',
     },
   ];
 
